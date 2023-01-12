6f402a6455bb574dff4ea7752ccd76690d2ca789
mickymacirl <mickymacster@gmail.com>
2023-01-12 21:34:47 +0000

    add about.html, add contact.html, add views and urls, fix urls in header and footer

--- BLOG/urls.py
+++ BLOG/urls.py
@@ -5,6 +5,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
 
 # This is a list of URL patterns for the blog app.
--- BLOG/views.py
+++ BLOG/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from django.views import generic
 from django.views.generic import ListView
 
 class PostList(ListView):
     template_name = 'index.html'
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
