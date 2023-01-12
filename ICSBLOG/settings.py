fd13842c038ada1eca4f5e9c2beb2ac86efe2571
mickymacirl <mickymacster@gmail.com>
2023-01-12 23:21:16 +0000

    test fix for cloudinary

--- ICSBLOG/settings.py
+++ ICSBLOG/settings.py
@@ -44,9 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-#    'cloudinary_storage',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
-#    'cloudinary',
+    'cloudinary',
     'BLOG',
 ]
 
@@ -131,16 +131,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'ds197sglr',
+    'API_KEY': '386852544353939',
+    'API_SECRET': '12edWFkIggh8fDyL73YaOcWuZAU',
+}
+
 STATIC_URL = '/static/'
 
-# STATICFILES_STORAGE = 'cloudinary_storage.storage.
-# StaticHashedCloudinaryStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 # Default primary key field type
