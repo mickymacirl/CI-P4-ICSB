fc0b5a06dfffc8bdb4a43ba12a14699c7ea2f7f0
mickymacirl <mickymacster@gmail.com>
2023-01-12 21:48:46 +0000

    remove cloudinary test

--- ICSBLOG/settings.py
+++ ICSBLOG/settings.py
@@ -44,9 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
+#    'cloudinary_storage',
     'django.contrib.staticfiles',
-    'cloudinary',
+#    'cloudinary',
     'BLOG',
 ]
 
@@ -133,12 +133,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 # Default primary key field type
