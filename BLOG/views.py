1a0ae4a7cb129e5ac20fea4609b603845dbfa160
mickymacirl <mickymacster@gmail.com>
2023-01-19 15:09:07 +0000

    add comments to views.py

--- BLOG/views.py
+++ BLOG/views.py
@@ -2,23 +2,22 @@ from django.shortcuts import render
 from django.views import generic
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-# CRUD
+"""
+CRUD
+"""
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, Comment
 from .forms import CommentForm
 
-# "This is a list view of the Post model,
-# and it will be rendered using the template
-# index.html."
-# The template_name attribute is used to tell
-# Django which template to use for a view.
-# In this case, we're using the template index.html
-
-
-# All Posts
 class AllPostView(ListView):
+    """
+    This class is a list view of all the posts in the database, and it will 
+    paginate the results by 5 posts per page."
+    URL for this view.:
+    url(r'^all/$', AllPostView.as_view(), name='all_posts'),
+    """
     model = Post
     template_name = 'all_posts.html'
     context_object_name = 'all_posts'
@@ -27,7 +26,8 @@ class AllPostView(ListView):
 
 def about(request):
     """
-    It takes a request object as an argument, and returns a response object
+    About Page
+    Takes a request object as an argument, and returns a response object
     :param request: This is the request object that is sent to the view
     :return: The about.html file is being returned.
     """
@@ -36,20 +36,22 @@ def about(request):
 
 def contact(request):
     """
-    It takes a request, and returns a response
+    Contact Page
+    Takes a request, and returns a response
     :param request: This is the request object that is sent to the view
     :return: The contact.html file is being returned.
     """
     return render(request, 'contact.html')
 
 
-# This view will return a list of posts that are published and ordered by the
-# most recently published post.
-# The first line of the class defines the model that will be used to retrieve
-# objects for the view.
-
-# Home Page View
 class PostList(generic.ListView):
+    """
+    Home Page View
+    This view will return a list of posts that are published and ordered by the
+    most recently published post.
+    The first line of the class defines the model that will be used to retrieve
+    objects for the view.
+    """
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
@@ -60,7 +62,7 @@ class PostList(generic.ListView):
 
 def post_detail(request, slug):
     """
-    It's rendering the template with the context
+    Rendering the template with the context
     :param request: The current request object
     :param slug: The slug is the URL-friendly version of the title
     :return: The template is being rendered with the context.
@@ -70,21 +72,30 @@ def post_detail(request, slug):
     comments = post.comments.filter(active=True)
     new_comment = None
 
-    # Comment posted
+    """
+    Comment posted
+    """
     if request.method == 'POST':
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
-
-            # Create Comment object but don't save to database yet
+            """
+            Create Comment object but don't save to database yet
+            """
             new_comment = comment_form.save(commit=False)
-            # Assign the current post to the comment
+            """
+            Assign the current post to the comment
+            """
             new_comment.post = post
-            # Save the comment to the database
+            """
+            Save the comment to the database
+            """
             new_comment.save()
     else:
         comment_form = CommentForm()
 
-# It's rendering the template with the context.
+    """
+    Rendering the template with the context.
+    """
     return render(request, template_name, {'post': post,
                                            'comments': comments,
                                            'new_comment': new_comment,
@@ -92,23 +103,49 @@ def post_detail(request, slug):
 
 
 def custom_403(request, exception):
+    """
+    Takes the request and exception as arguments, and returns a rendered 
+    template  
+    :param request: The request object
+    :param exception: The exception raised by the view
+    :return: A 403 error page.
+    """
     return render(request, '403.html')
 
 
-# in urls.py
+"""
+specify a custom view for a 403 error.
+"""
 handler403 = 'path.to.custom_403'
 
 
 def handle_500(request):
+    """
+    Takes a request object as an argument, and returns a response object that 
+    renders the 500.html template
+    :param request: The request object used to generate this response
+    :return: The 500.html page is being returned.
+    """
     return render(request, '500.html')
 
-# Create, Read, Update, Delete
 
 def get_success_url(self):
+    """
+    Returns the URL of the detail page for the object that was just created
+    :return: The reverse_lazy function is being used to return the url of the 
+    post_detail view.
+    """
     return reverse_lazy('post_detail', kwargs={'slug': self.object.slug})
 
 
 class PostListView(ListView, LoginRequiredMixin):
+    """
+    List view of Post objects, and it's called PostListView.
+    The first line of the class tells Django to use the ListView generic view. 
+    The next line tells# Django to use the post_list.html template. 
+    The last line tells Django to use the context name posts
+    for the list of posts
+    """
     model = Post
     template_name = 'post_list.html'
     context_object_name = 'posts'
@@ -133,7 +170,6 @@ class PostUpdateView(UpdateView, LoginRequiredMixin):
     fields = ['author', 'title', 'content', 'category', 'status', 'is_pinned']
     context_object_name = 'post'
     success_url = reverse_lazy('post_list')
-    # success_url = get_success_url
 
 
 class PostDeleteView(DeleteView, LoginRequiredMixin):
