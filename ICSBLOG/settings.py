341d625f192b013f50b971d7b54486b3c1c03f88
mickymacirl <mickymacster@gmail.com>
2023-01-20 03:42:33 +0000

    commit before deploy

--- ICSBLOG/settings.py
+++ ICSBLOG/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ["icsblogp4.herokuapp.com", "localhost"]
 
